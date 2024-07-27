5f9d0fc2c4c7cff77736c2f512f396880363789d
tchz <theodoros.chatzi@tremau.com>
2024-07-27 22:47:56 +0300

    minor fixes

--- flake.nix
+++ flake.nix
@@ -3,28 +3,20 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
     emacs-overlay.url = "github:nix-community/emacs-overlay";
+    
   };
 
-  outputs = { self, nixpkgs, emacs-overlay }:
-    let
-      pkgs = import nixpkgs { system = "x86_64-linux"; overlays = [ emacs-overlay.overlays.emacs ]; };
+  outputs = { self, nixpkgs, emacs-overlay, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+      pkgs = import nixpkgs { system = system; overlays = [ emacs-overlay.overlays.emacs ]; };
       emacs-with-org = (pkgs.emacsPackagesFor pkgs.emacs)
         .emacsWithPackages (
           ps: [ps.org]
         );
-    in
-    {
-
-    # packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    # packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-      devShells."x86_64-linux".default = pkgs.mkShell {
-        buildInputs = [ (pkgs.emacs) ];
-      };
-
-      packages."x86_64-linux".configured-emacs =
-        (pkgs.emacsWithPackagesFromUsePackage {
+      emacs-with-config = (pkgs.emacsWithPackagesFromUsePackage {
           config = ././conf.org;
 
           alwaysTangle = true;
@@ -35,5 +27,11 @@
             epkgs.use-package
           ];
         });
-      };
+    in
+      {
+        packages.default = emacs-with-config;
+        _overlays.default = (final: prev: {
+          emacs-with-config = emacs-with-config;
+        });
+      });
 }
